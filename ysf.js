9393f3f5a5c1908c660201fa51d4d2b0e6b4a168
xqllr <1242745988@qq.com>
2021-10-27 18:24:39 +0800

    运算符的测试

---
+++ ysf.js
@@ -0,0 +1,38 @@
+// let a = 1; 
+// let s = "我的" * a; 
+// console.log(s); 
+
+// let a = 10 * 10  %  2  + 18  / 3 ** 2;
+// console.log(a);
+
+// let s = 10;
+// s += s;
+// console.log(s);
+
+// let a = 10;
+// let s = a++;
+// console.log(a);
+
+/**
+  对象判断是比较内存地址
+  每生成一个实例就会重新占用一些内存
+ */
+
+// console.log(true  || false  &&  false);
+// console.log(10 == "10"  &&  "11" > 10  ||  false);
+
+// let obj = {
+//   name: "夏栀",
+//   age: 18,
+//   sex: "女",
+// }
+// let arr = [11]
+// let bol = obj.age == arr[0] || 66 
+// console.log(bol);
+
+// let a = "" || "123" === 123 || function(){} && [ ] || 0 
+// console.log(a);
+
+let a = 1;
+let b = '1'
+console.log(a!==b);
